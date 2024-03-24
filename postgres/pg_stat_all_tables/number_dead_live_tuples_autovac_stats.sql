4f9f191a271a65490dd390e607894855b33c3cdf
sferg007 <78802916+sferg007@users.noreply.github.com>
2024-03-24 13:24:45 +0300

    Create number_dead_live_tuples_autovac_stats.sql

---
+++ postgres/pg_stat_all_tables/number_dead_live_tuples_autovac_stats.sql
@@ -0,0 +1,16 @@
+SELECT
+schemaname,
+relname,
+n_live_tup,
+n_dead_tup,
+n_dead_tup * 100 / (case when n_live_tup > 0 then n_live_tup else 1 end) as dead_rows_percent,
+last_autovacuum,
+last_autoanalyze,
+n_dead_tup,
+relname
+from
+pg_stat_all_tables
+WHERE
+schemaname not in ('pg_catalog','information_schema','pg_toast')
+ORDER BY
+n_dead_tup DESC;
